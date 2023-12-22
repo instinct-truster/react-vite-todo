4ef0ba3eedba23cc3099c4c997588525ca86b672
Lance Turbes <instinct_truster@protonmail.com>
2023-12-22 14:46:32 -0800

    Complete react todo app working

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,38 @@
+import spotify from "../../assets/spotify.svg";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import styles from "./header.module.css";
+import { useState } from "react";
+
+export function Header({ onAddTask }) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    onAddTask(title);
+    setTitle("");
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <img src={spotify} />
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input
+          placeholder="add a new task"
+          type="text"
+          value={title}
+          onChange={onChangeTitle}
+        />
+        <button>
+          Create
+          <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
+    </header>
+  );
+}
